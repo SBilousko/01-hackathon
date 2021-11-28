28b898efd956fa7118f8ad3be9ad6c2d7c10fcef
IgorSerebr17 <38539739+IgorSerebr17@users.noreply.github.com>
2021-11-28 19:59:30 +0300

    Update lettering.module.js

--- src/modules/lettering.module.js
+++ src/modules/lettering.module.js
@@ -20,10 +20,7 @@ export class LetteringClass extends Module {
             let textStyle = Math.floor(Math.random() * stylesArr.length);
             let text = Math.floor(Math.random() * letteringsArr.length);
             let x = Math.floor(Math.random() * WIDTH);
-            console.log('x = ', x);
-
             let y = Math.floor(Math.random() * HEIGHT);
-            console.log('y = ', y);
             return [textStyle, text, x, y];
         }
         function create(randomArr) {
