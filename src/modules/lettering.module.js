b0aa1de883d84f954ac4654fd993c408b6715a6e
IgorSerebr17 <38539739+IgorSerebr17@users.noreply.github.com>
2021-11-28 18:31:39 +0300

    Update lettering.module.js

--- src/modules/lettering.module.js
+++ src/modules/lettering.module.js
@@ -1,20 +1,11 @@
 import {Module} from '../core/module'
 
 export class LetteringClass extends Module {
-    trigger() {
         const letteringsArr = ["JavaScript", "Node.js", "React", "Angular", "HTML", "CSS", "jQuery", "Vue.js"];
         const stylesArr = ["textStyle1", "textStyle2", "textStyle3", "textStyle4", "textStyle5"];
 
         const WIDTH = 900;
         const HEIGHT = 800;
-
-        // const wrapper = document.createElement('div');
-        // //wrapper.style.background = 'Honeydew'
-        // wrapper.style.position = 'relative'
-        // wrapper.style.width = `${WIDTH}px`
-        // wrapper.style.height = `${HEIGHT}px`
-        // wrapper.style.margin = '0 auto'
-        // document.body.append(wrapper);
 
         const dataArr = randomCount();
         screenText = create(dataArr);
@@ -37,19 +28,9 @@ export class LetteringClass extends Module {
             return [textStyle, text, x, y];
         }
         function create(randomArr) {
-            //const textM = document.createElement('span');
             const textSpan = document.createElement('span');
-
             textSpan.textContent = letteringsArr[randomArr[1]];
             textSpan.classList.add(`${stylesArr[randomArr[0]]}`);
-
-            // if (randomArr[0] === 0) {
-            //     textM.classList.add(`${stylesArr[1]}`);
-            //     textM.textContent = 'Наведите курсор на текст)';
-            //     textM.style.top = 450 + "px";
-            //     textM.style.left = 450 + "px";
-            //     document.body.append(textM);
-            // }
             textSpan.style.top = randomArr[3] + "px";
             textSpan.style.left = randomArr[2] + "px";
             document.body.append(textSpan);
@@ -62,4 +43,4 @@ export class LetteringClass extends Module {
 
 
             }
-        }
+}
